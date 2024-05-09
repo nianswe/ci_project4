f8e161d0eff7b2444b29bc7450727b4a470508f1
Niklas Andersson <nianswe@hotmail.com>
2024-05-09 13:42:50 +0000

    Prepared for Heroku deployment

--- stockhandler/settings.py
+++ stockhandler/settings.py
@@ -10,22 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Import env if env.py exists
+if os.path.exists('env.py'):
+    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z$$ori^==!-x0$2_driqe^v3@my0$23&f#s0yeaxk6w9@9^jdu'
+# Get SecretKey from environment
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = 'DEVELOPMENT' in os.environ
+
+ALLOWED_HOSTS = [,'.herokuapp.com']
 
 
 # Application definition
